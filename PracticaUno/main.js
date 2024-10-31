31366afd3b3cbb9841ce5ba4d1027e2587a41bd2
我操 <adriandominguezinfante1999@gmail.com>
2024-10-31 09:31:04 +0100

    AnadirTareas

--- PracticaUno/main.js
+++ PracticaUno/main.js
@@ -1,24 +1,71 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const divs = document.querySelectorAll("div");
+const btns = document.querySelectorAll("button");
+const input = document.querySelectorAll("input")[0];
+const ul = document.querySelectorAll("ul")[0];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// console.log(ul);
 
-setupCounter(document.querySelector('#counter'))
+const container = divs[0];
+container.className = 'container';
+
+const searchContainer = divs[1];
+searchContainer.className = 'search';
+
+const liContainer = divs[2];
+liContainer.className = 'li-container';
+
+const emptyContainer = divs[3];
+emptyContainer.className = 'empty';
+
+const taskCountContainer = divs[4];
+taskCountContainer.className = 'task-count';
+
+const btnAnadir = btns[0];
+btnAnadir.className = 'btn-add';
+
+// const 
+// botones.forEach(element => {
+//   element.setAttribute("style", "background-color: black; border-color: #f6f6f6; color: #f6f6f6");
+// });
+
+
+// function anadirTarea() {
+
+// }
+
+
+const noTaskSpan = emptyContainer.querySelectorAll("p")[0];
+const taskCounter = taskCountContainer.querySelectorAll("span")[1];
+
+console.log(taskCounter.className);
+
+btnAnadir.addEventListener("click", function (e){
+  e.preventDefault(); 
+
+  const textoTarea = input.value;
+  // console.log(textoTarea);
+
+  crearTareas(ul, textoTarea);
+  
+  // console.log(liContainer);
+})
+
+function crearTareas(ul, texto) {
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+	const span = document.createElement("span");
+
+  const btnDelete = document.createElement("button");
+  btnDelete.className = 'btn-delete';
+  btnDelete.textContent = 'x';
+
+  ul.appendChild(li)
+  li.appendChild(p);
+  li.appendChild(btnDelete)
+  p.appendChild(span);
+  
+  span.textContent = texto;
+  noTaskSpan.remove();
+
+  return ul;
+}
